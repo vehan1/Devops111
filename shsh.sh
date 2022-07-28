5a6daaa7454f417d66e0b42deb01ce63bb76df26
vehan1 <usha.gangavarapu1@gmail.com>
2022-07-28 05:22:02 -0700

    2

--- shsh.sh
+++ shsh.sh
@@ -2,5 +2,25 @@
 mkdir Task1
 echo "I am creating directory"
 mkdir Task1
+<<<<<<< HEAD
 echo "hi there" 
 echo "hello"
+=======
+echo "hi baby"
+echo "creating files "
+
+for((i=1; i<=5; i++ ))
+do
+        echo "welcome $i"
+        touch task_$i.txt
+done
+
+ls -l
+        echo "renaming files with current date"
+
+for((i=1; i<=5; i++))
+do
+        mv task_${i}.txt task_${i}_$(date '+%Y-%m-%d').txt
+do
+echo "hi ma baby"
+>>>>>>> 9fc6322 (2)
